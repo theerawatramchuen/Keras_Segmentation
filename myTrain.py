cf4ba9c90a222b4a1e69e6a6b22c7882da2f92c5
Theerawat Ramchuen <ramchuen@gmail.com>
2019-07-14 13:57:46 +0700

    Add files via upload

--- myTrain.py
+++ myTrain.py
@@ -10,8 +10,8 @@ import keras_segmentation
 model = keras_segmentation.models.unet.vgg_unet(n_classes=51 ,  input_height=416, input_width=608  )
 
 model.train( 
-    train_images =  "dataset1/images_prepped_train/",
-    train_annotations = "dataset1/annotations_prepped_train/",
+    train_images =  "dataset1/images_prepped_train",
+    train_annotations = "dataset1/annotations_prepped_train",
     checkpoints_path = "tmp/vgg_unet_1" , epochs=5
 )
 
@@ -20,6 +20,9 @@ out = model.predict_segmentation(
     out_fname="tmp/out.png"
 )
 
-
 import matplotlib.pyplot as plt
 plt.imshow(out)
+import cv2
+import numpy
+inImg = numpy.asarray(cv2.imread("dataset1/images_prepped_test/0016E5_07965.png"))
+plt.imshow(inImg)
